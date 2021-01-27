be5a7e9277df7b8dfa33add4a26366e7b2c3112a
ThiagooKings <thiagomreeis@gmail.com>
2021-01-27 15:23:34 -0300

    segunda aula faltando desafio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -12,40 +16,68 @@ import QuizBackground from '../src/components/QuizBackground'
 //   background-size: cover;
 //   background-position: center;
 // `;
+
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
-  @media screen and (max-width:500px) {
+  @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
   return (
-      <QuizBackground backgroundImage ={db.bg} >
-        <QuizContainer>
-          
-          <Widget>
-            <Widget.Header>
-                <h1>One Piece Quiz</h1>
-              </Widget.Header>
-            <Widget.Content>
-                <p>Loren inpsu dolor allslssl</p>
-            </Widget.Content>
-          </Widget>    
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da Galera</h1>
-              <p>Loren inpsu dolor allslssl</p>
-            </Widget.Content>
-          </Widget>
-        <Footer/>
-        </QuizContainer>
-        <GitHubCorner projectUrl= 'https://github.com/ThiagooKings'/>
-      </QuizBackground>
-    );
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.head}</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão pelo react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
 }
